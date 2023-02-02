4f795035b7dce38c27a6c29132a1cf22eef34b48
CMWolfe <cmwolfe1123@gmail.com>
2023-02-02 17:47:10 -0600

    Initial Commit

---
+++ components.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import sqlite as sql
+from sqlalchemy import create_engine
+import sqlalchemy as sa
+
+
+
+ENV_PATH = Path('/config/.env')
+
+
+class Storage:
+
+	def __init__(self, database:str, *args, **kwargs):
+		self.db = database
+		self.args = args
+		self.kwargs = kwargs
+
+	

000d809fe7d136c01c64c743931bd1a523d76c64
CMWolfe <cmwolfe1123@gmail.com>
2023-02-04 07:31:18 -0600

    Created two openai functions in the ai folder in the components file.

--- ai/components.py
+++ ai/components.py
@@ -4,13 +4,13 @@ import openai
 # openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def complete_text(
-    model:str = "text-davinci-003", 
-    prompt:str = "", 
+    model:str = "text-davinci-003",
+    prompt:str = "",
     max_tokens:int = 256,
     top_p:int = 1,
     frequency_penalty:int = 0,
     presence_penalty:int = 0
-    )
+    ):
     
     """The prompt is the most important input."""
 
@@ -27,13 +27,22 @@ def complete_text(
     return response
 
 
-def generate_image(prompt:str, size:str = "500x500", num:int = 1):
+def generate_image(prompt:str, size:str = "256x256", num:int = 1):
 
     response = openai.Image.create(
       prompt=prompt,
       n=num,
       size=size
     )
-    image_url = response['data'][0]['url']
-    print(image_url)
-    return image_url
+
+    if len(response['data']) == 1:
+
+        image_url = response['data'][0]['url']
+        print(image_url)
+        return image_url
+    else:
+        data = dict()
+        for i in range(len(response['data'])):
+            image_url = response['data'][i]['url']
+            data[response['data']]
+
